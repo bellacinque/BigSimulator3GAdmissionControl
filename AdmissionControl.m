65aecbd6f22168a6dc46d5dc0d39efa8a470a14a
bellacinque <pasqualecr7@gmail.com>
2017-09-14 17:05:16 +0200

    Lettera iniziale maiuscola dei file

---
+++ AdmissionControl.m
@@ -0,0 +1,169 @@
+%% ADMISSION CONTROL 
+%Due cicli: 1)ammissione BS per BS con ext
+%             interference iniziale zero
+%           2)ammissione considerando l'interferenza esterna
+%             degli utenti ammessi alla rete nel ciclo 1
+
+%%
+%Primo ciclo di admission control: interferenza inziale zero
+Itot = zeros(37,1);    
+Ctot = zeros(37,1);
+count = zeros(37,1);
+Pnoise = zeros(37,1);
+ 
+%Potenza di rumore singolo utente
+Pn = 1.38 * 10^-15;  %%Pn= k*Tsys*Rb where k = 1.38*10^-23, Tsys = 1000?K and Rb = 100 Kbit/s for video traffic only.
+     
+%Potenza ricevuta utenti ammessi BS 
+v = randperm(nBs);                          %Vettore permuta indici BS
+for i=1:nBs
+    %Selezione utenti accampati in BS i
+    ind = find(Users(:,2)==v(i));
+    UsersCell= Users(ind, :);               %Matrice degli utenti accampati in BS v(i)
+    
+    %Calcolo interferenza ricevuta dagli utenti ammessi nelle altre BSs
+    Interferers = find(Users(:,7)==1); 
+    if (~isempty(Interferers))
+        Itot(v(i)) = sum(Pr_withShAllUsers(Interferers,v(i)));
+        Ctot(v(i)) = 0;
+        Pnoise(v(i)) = Pn;
+        loadF = 0;
+        n=1;
+    else
+        Users(UsersCell(1,1),7) = 1;        %primo utente sempre ammesso
+        Ctot(v(i)) = UsersCell(1,6);
+        Pnoise(v(i)) = Pn;
+        Ptot = UsersCell(1,6) + Itot(v(i)) + Pnoise(v(i));
+        Itot(v(i)) = UsersCell(1,6);
+        loadF = UsersCell(1,6)/Ptot;
+        n=2;
+    end
+
+    count(v(i))=0;
+    for k=n:length(ind)
+        if count(v(i))<15
+            %%calcolo interferenza causata da utente k
+            deltaI = UsersCell(k,6)/(1-loadF);
+            if Itot(v(i))+deltaI < Imax
+                %user k is admitted
+                Users(UsersCell(k,1),7) = 1;
+                Itot(v(i)) = Itot(v(i))+UsersCell(k,6);
+                Ctot(v(i)) = Ctot(v(i)) + UsersCell(k,6);
+                Pnoise(v(i)) = Pnoise(v(i)) + Pn;
+                Ptot = Itot(v(i)) + Pnoise(v(i));
+                loadFj = UsersCell(k,6)/Ptot;
+                loadF = loadF + loadFj;
+                count(v(i)) = count(v(i))+1;
+            end
+        end
+    end
+end
+
+%%     
+%Secondo ciclo: intererferenza iniziale uguale all'interferenza di tutti gli utenti ammessi alla rete nel ciclo 1
+    
+v = randperm(nBs);                                %Vettore permuta indici BS  
+for i=1:nBs
+    %Selction of rows correspondent to BS v(i)
+    ind = find(Users(:,2)==v(i)); 
+    UsersCell= Users(ind, :);                     %Matrice degli utenti accampati in BS i
+  
+    %Calcolo interferenza ricevuta dagli utenti ammessi nelle altre BSs
+    Interferers = find(Users(:,7)==1);
+   if (~isempty(Interferers))
+    Itot(v(i)) = sum(Pr_withShAllUsers(Interferers,v(i)));
+   end
+    
+    for k=1:length(ind)
+        if count(v(i))<15
+            %%calcolo interferenza causata da utente k
+            if Users(UsersCell(k,1),7)==0
+                Ptot = UsersCell(k,6) + Itot(v(i)) + Pnoise(v(i)) + Pn;
+                Ctot(v(i)) = Ctot(v(i)) + UsersCell(k,6);
+            else
+                Ptot = Itot(v(i)) + Pnoise(v(i));
+            end
+            loadF = Ctot(v(i))/Ptot;
+            deltaI = UsersCell(k,6)/(1-loadF);
+            if Itot(v(i))+deltaI < Imax
+                %user k is admitted
+                if Users(UsersCell(k,1),7)==0   	% se non era ammesso nel ciclo 1 aumento gli indicatori
+                    Users(UsersCell(k,1),7) = 1;
+                    Itot(v(i))=Itot(v(i))+ UsersCell(k,6);
+                    %Ctot(v(i)) = Ctot(v(i)) + UsersCell(k,6);
+                    Pnoise(v(i))=Pnoise(v(i)) + Pn;
+                    count(v(i))=count(v(i))+1;
+                end
+            else
+                % user k is non admitted
+                  Ctot(v(i)) = Ctot(v(i)) - UsersCell(k,6);
+                if Users(UsersCell(k,1),7)==1       % se era stato ammesso nel ciclo 1 decremento gli indicatori
+                    Users(UsersCell(k,1),7)= 0;
+                    Itot(v(i)) = Itot(v(i)) - UsersCell(k,6);
+                    Pnoise(v(i)) = Pnoise(v(i))-Pn;
+                    count(v(i))=count(v(i))-1;
+                end
+                    
+            end
+        else
+            Users(UsersCell(k,1),7) = 0;
+        end
+    end
+end
+    
+%
+%%Directed retry
+
+ind = find(Users(:,7)==0);
+UsersNotAd = Users(ind,:);
+for i=1:length(UsersNotAd(:,1))
+    [max,indBS] = sort((Pr_withShAllUsers(UsersNotAd(i,1),:)),'descend'); %%ordina potenze ricevute alla BS da utente  
+    
+    for j=1:4
+        if (indBS(j)~=UsersNotAd(i,2))
+        if count(indBS(j))<15
+        %%calcolo interferenza causata da utente k
+        Ptot = Itot(indBS(j)) + Pnoise(indBS(j));                         %Ptot= potenza ricevuta utente k + Itot + Pnoise
+        loadF = Ctot(indBS(j))/Ptot;
+        deltaI = max(j)/(1-loadF);
+        %Calcolo interferenza ricevuta dagli utenti ammessi nelle altre BSs
+        Interferers = find(Users(:,7)==1);
+        Itot(indBS(j)) = sum(Pr_withShAllUsers(Interferers,indBS(j)));
+        
+        if Itot(indBS(j))+deltaI < Imax
+         Users(UsersNotAd(i,1),7) = 1;      % utente ammesso
+         Users(UsersNotAd(i,1),2) = indBS(j);  % utente accampato alla nuova BS
+         Pnoise(indBS(j))=Pnoise(indBS(j)) + Pn;
+         count(indBS(j))=count(indBS(j))+1;
+         Ctot(indBS(j)) = Ctot(indBS(j)) + max(j);
+         break;
+         end
+        end
+        end
+    end
+    
+end
+
+
+
+%%
+%%%Calcolo C/I di ogni utente, risultato in colonna 8 di "Users"
+      Interferers = find(Users(:,7)==1);
+
+  for j=1:37
+     %Selction of rows correspondent to BS j
+     ind = find(Users(:,2)==j);
+     UsersCell = Users(ind, :);           %Matrice degli utenti accampati in BS i
+     %Calcolo interferenza ricevuta dagli utenti ammessi
+     Itot1= sum(Pr_withShAllUsers(Interferers,j));
+     for k=1:length(ind)
+         %%Calcolo C/I utente
+         Users(UsersCell(k,1),8) = abs(Users(UsersCell(k,1),6)/(Itot1-Users(UsersCell(k,1),6)));
+     end
+  end
+   
+%%
+
+
+        
+        
---
+++ UsersGeneration.m
@@ -0,0 +1,69 @@
+%% Generazione utenti
+
+%meanUsers = 32;                                             %numero medio utenti per esagono                                                %numero di esagoni
+radiusTot = 7*(sqrt(3)/2*radius);
+
+areaQ = (2*radiusTot)^2;                                    %area quadrato su cui vengono generati gli users
+areaHex = (radius^2)*3*sqrt(3)/2;                           %area singolo esagono
+areaCircle = pi*radius^2;                                   %area cerchio circoscritto esagono
+areaPlus= (areaCircle-areaHex)*42;                          %42 numero lati esagono esterni
+areaTot = (nBs*areaHex)+areaPlus;                           %area totate esagoni + area esterna
+
+nUsers = round(areaQ/areaErl);                                %n.utenti fissata la densit?
+%nUsers = round((meanUsers*nBs)*areaQ/areaTot);  %numero di users da generare per avere 32users di media all'interno degli esagoni
+
+% generazione su un quadrato
+usersPositionX = -radiusTot + (radiusTot-(-radiusTot)).*rand(nUsers,1);
+usersPositionY = -radiusTot + (radiusTot-(-radiusTot)).*rand(nUsers,1);
+
+% generazione su un cerchio
+%angle = (2*pi).*rand(1184,1);
+%r = -radiusTot + (2*radiusTot).*rand(1184,1);
+%usersPositionX=r.*cos(angle); 
+%usersPositionY=r.*sin(angle);
+
+%plot(usersPositionX,usersPositionY,'o');
+
+%% Calcolo distanze di ogni utente da tutte le base stations e scarto degli utenti fuori dagli esagoni
+
+D       =   zeros(nUsers,nBs);
+index   =   ones(nUsers,1);
+
+% MATRICE USERS
+% Matrice con: numero utente|Bs in cui ? accampato|distanza da Bs in cui ?
+%%accampato |Pr before PC|Ptx after PC|Pr|admission y or n 
+Users = zeros(nUsers,8);
+
+for i=1:nUsers
+    cellIndex=1;
+    minDistance= radius;
+    for j=1:nBs
+        D(i,j)=sqrt((abs(usersPositionX(i)-X(j)))^2+(abs(usersPositionY(i)-Y(j)))^2);
+        if D(i,j)< minDistance
+            minDistance=D(i,j);
+            cellIndex = j;
+            index(i)=0;                                     %indice utenti buoni
+        end
+    end
+    Users(i,2)= cellIndex;             %Salvo indice BS
+    Users(i,3)= minDistance;           %Salvo distanza utente-BS
+end
+
+index = logical(index);                                     %vettore convertito in boolean   
+
+D(index,:)=[];                                              %scarto dalla matrice gli utenti indesiderati
+nUsers=length(D);                                           %aggiorno numero corretto utenti
+Users(index,:)=[];
+Users(:,1)= 1:nUsers;
+
+usersPositionX(index)=[];                                   %scarto utenti con index(i)=1
+usersPositionY(index)=[];
+
+%plot(usersPositionX,usersPositionY,'o');
+
+
+
+
+
+
+
