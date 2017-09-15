050ff284b0aaa4756b7b22ee279ed38c347f8a7a
michelemen <michele@MBPdiMicheleM.homenet.telecomitalia.it>
2017-09-15 17:24:27 +0200

    PlotCurves modificato

--- PlotCurves.m
+++ PlotCurves.m
@@ -2,20 +2,20 @@ close all    % Close all open figures
 clear        % Reset variables
 clc          % Clear the command window
 
-Data=load('KPIptMinLimited_10users_noDirectedRetry.txt');
+Data=load('1000_8users.txt');
 % Data=sortrows(Data,1);
 
 figure1 = figure(1);
-plot(Data(:,1),Data(:,2)/100);
+plot(Data(:,1),Data(:,2)/100, 'o','LineStyle','-');
 
 hold on; 
 grid on;
 
-plot(Data(:,1),Data(:,3)/100);
+plot(Data(:,1),Data(:,3)/100,'+','LineStyle','-');
 
-xlabel('I_{max} [W]');
-ylabel('Adm / Out');
+xlabel('I_{max} [W]', 'FontSize', 13);
+ylabel('Admission / Outage rate','FontSize', 13);
 legend('Admission rate', 'Outage rate');
 
-    title(strcat('R = ', num2str(Data(1,6)),' m, Average users per cell = ', num2str(Data(1,5)), ', Pt_{min} = 4*10^8 W'));
+    title(strcat('R = ', num2str(Data(1,6)),' m, Average users per cell = ', num2str(Data(1,5))));
    
